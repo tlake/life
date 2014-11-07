6d53fda7b195ad1867ce0625a35772ba07641544
Stephanie Zolayvar <swz@swz-macbookpro.roam.corp.google.com>
2014-11-06 23:13:00 -0800

    Nooooo you made me use an if statment

--- butt.py
+++ butt.py
@@ -1,10 +1,13 @@
 
 def make_next_board(thing):
-  return [
+  out = [
     ['0', '0', '0'],
     ['0', '1', '0'],
     ['0', '0', '0']
   ]
+  if thing[0][2] == '1':
+    out[1][1] = '0'
+  return out
 
 
 def sum_neighbors(thing_1, thing_2, thing_3):
