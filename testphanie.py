f4df829c5253b85317516c3e4a19243ff2428d53
tlake <tanner.lake@gmail.com>
2014-11-06 23:27:22 -0800

    try a bigger board now

--- testphanie.py
+++ testphanie.py
@@ -22,8 +22,21 @@ starting_board_3 = [
     ['0','1','0'],
     ['0','0','0']
 ]
+starting_board_4 = [
+    ['0','0','0','1','0'],
+    ['0','1','0','0','0'],
+    ['0','0','0','0','0'],
+    ['0','0','0','0','0'],
+    ['1','1','1','1','1']
+]
 
-
+ending_board_4 = [
+    ['0','0','0','0','0'],
+    ['0','0','0','0','0'],
+    ['0','0','0','0','0'],
+    ['0','0','0','0','0'],
+    ['0','1','1','1','0']
+]
 ending_board_0 = [
     ['0','0','0'],
     ['0','1','0'],
@@ -53,6 +66,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(make_next_board(starting_board_1), ending_board_1)
         self.assertEqual(make_next_board(starting_board_2), ending_board_2)
         self.assertEqual(make_next_board(starting_board_3), ending_board_3)
+        self.assertEqual(make_next_board(starting_board_4), ending_board_4)
 
 if __name__ == '__main__':
     unittest.main()
