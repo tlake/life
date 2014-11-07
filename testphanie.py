5f8b1c6f9c28d0b5b1caea61ea75e07e2413a9a6
tlake <tanner.lake@gmail.com>
2014-11-07 01:46:31 -0800

    testing for 2x2 squares (test may need testing)

--- testphanie.py
+++ testphanie.py
@@ -33,6 +33,8 @@ starting_board_4 = [
     ['1','1','1','1','1']
 ]
 
+print "Stephanie's a big ol' butt."
+
 ending_board_4 = [
     ['0','0','0','0','0'],
     ['0','0','0','0','0'],
@@ -61,6 +63,15 @@ ending_board_3 = [
     ['0','0','0']
 ]
 
+# SHAPES/FOOTPRINTS
+# this is the footprint for a 2x2 shape (with 4x4 footprint)
+footprint_square = [
+    ['0','0','0','0'],
+    ['0','1','1','0'],
+    ['0','1','1','0'],
+    ['0','0','0','0']
+]       
+
 
 class MyTest(unittest.TestCase):
     def test(self):
@@ -75,14 +86,33 @@ class MyTest(unittest.TestCase):
 
         # make a randomly-sized board
         rando_board = []
-        size_1 = random.randint(3, 10)
-        size_2 = random.randint(3, 10)
-        for i in range(size_1):
-            rando_board.append(['0'] * size_2)
+        num_rows = random.randint(8, 20)
+        num_cols = random.randint(8, 20)
+        for i in range(num_rows):
+            rando_board.append(['0'] * num_cols)
+        # test for randomly-sized boards
+        self.assertEqual(len(make_next_board(rando_board)), num_rows)
+        self.assertEqual(len(make_next_board(rando_board)[0]), num_cols)
+
+        # randomly select a start location for a 2x2 immortal shape
+        # (this has a 4x4 footprint)
+        start_row = random.randint(0, num_rows - 4)
+        start_col = random.randint(0, num_cols - 4)
+        # insert a 2x2 square somewhere in the board
+        for row in range(len(footprint_square)):
+            for cell in range(len(footprint_square[row])):
+                rando_board[start_row + row][start_col + col] \
+                    = footprint_square[row][col]
+        # test that the randomly-located 2x2 is in the same
+        # spot after the next board has been generated
+        for row in range(1, 3):
+            for col in range(1, 3):
+                self.assertEqual(make_next_board(rando_board)[start_row \
+                    + row][start_col + col], '1')
         
-        self.assertEqual(len(make_next_board(rando_board)), size_1 )
-        self.assertEqual(len(make_next_board(rando_board)[0]), size_2)
 
 if __name__ == '__main__':
     unittest.main()
+
+
 
