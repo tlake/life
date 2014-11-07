4a6a6df788a0bde10f843bfb997619af25197f8b
tlake <tanner.lake@gmail.com>
2014-11-06 22:36:24 -0800

    gitsharing yay

---
+++ testphanie.py
@@ -0,0 +1,15 @@
+import unittest
+from life import *
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(4, 4)
+        a = sum_neighbors([['1','1','0'],['0','0','0'],['0','0','0']], 1, 1)
+        self.assertEqual(a, 2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
